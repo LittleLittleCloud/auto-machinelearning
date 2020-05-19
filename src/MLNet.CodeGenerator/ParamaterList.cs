ee8751a312fc49a1a0211fe275bbdcff2b4de79c
LittleLittleCloud <bigmiao.zhang@gmail.com>
2020-05-18 23:45:48 -0700

    add MLNet.CodeGenerator

---
+++ src/MLNet.CodeGenerator/ParamaterList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MLNet.CodeGenerator
+{
+    public class ParamaterList : List<Paramater>, ICodeGenNode
+    {
+        public string GeneratorCode()
+        {
+            return string.Join(",", this);
+        }
+    }
+}
