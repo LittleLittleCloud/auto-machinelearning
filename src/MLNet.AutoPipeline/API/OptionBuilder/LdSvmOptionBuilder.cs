a47ef0daaed4f52a69887337293adaff5a1966b8
Xiaoyun Zhang <bigmiao.zhang@gmail.com>
2020-08-05 22:09:41 -0700

    add ld svm and linear svm (#19)
    * add linear svm
    
    * add ld svm

---
+++ src/MLNet.AutoPipeline/API/OptionBuilder/LdSvmOptionBuilder.cs
@@ -0,0 +1,48 @@
+﻿// <copyright file="Class1.cs" company="BigMiao">
+// Copyright (c) BigMiao. All rights reserved.
+// </copyright>
+
+using Microsoft.ML.Trainers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MLNet.AutoPipeline
+{
+    public class LdSvmOptionBuilder : OptionBuilder<LdSvmTrainer.Options>
+    {
+        /// <summary>
+        /// The number of boosting iterations. A new tree is created in each iteration, so this is equivalent to the number of trees.
+        /// <para>Default sweeping configuration.</para>
+        /// <list type="bullet">
+        /// <item>
+        /// <term>type</term>
+        /// <description><c>Int</c></description>
+        /// </item>
+        /// <item>
+        /// <term>minimum value</term>
+        /// <description><c>10</c></description>
+        /// </item>
+        /// <item>
+        /// <term>maximum value</term>
+        /// <description><c>1000</c></description>
+        /// </item>
+        /// <item>
+        /// <term>log base</term>
+        /// <description><c>true</c></description>
+        /// </item>
+        /// <item>
+        /// <term>step</term>
+        /// <description><c>20</c></description>
+        /// </item>
+        /// </list>
+        /// </summary>
+        [SweepableParameter(10, 1000, true, 20)]
+        public int NumberOfIterations;
+
+        [SweepableParameter(1, 100, true, 20)]
+        public int TreeDepth;
+
+        internal static LdSvmOptionBuilder Default = new LdSvmOptionBuilder();
+    }
+}
